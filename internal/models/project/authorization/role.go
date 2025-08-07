1d6107314f3865e7a1a685b00235a914e3db1973
aviadl <aviad@descope.com>
2025-08-07 18:32:28 +0300

    Mark roles as private So we will not expose them to tenant admins related to https://github.com/descope/etc/issues/11663

--- internal/models/project/authorization/role.go
+++ internal/models/project/authorization/role.go
@@ -1,6 +1,7 @@
 package authorization
 
 import (
+	"github.com/descope/terraform-provider-descope/internal/models/attrs/boolattr"
 	"github.com/descope/terraform-provider-descope/internal/models/attrs/stringattr"
 	"github.com/descope/terraform-provider-descope/internal/models/attrs/strsetattr"
 	"github.com/descope/terraform-provider-descope/internal/models/helpers"
@@ -13,6 +14,8 @@ var RoleAttributes = map[string]schema.Attribute{
 	"name":        stringattr.Required(stringvalidator.LengthAtMost(100)),
 	"description": stringattr.Optional(stringattr.StandardLenValidator),
 	"permissions": strsetattr.Optional(),
+	"default":     boolattr.Optional(),
+	"private":     boolattr.Optional(),
 }
 
 type RoleModel struct {
@@ -20,6 +23,8 @@ type RoleModel struct {
 	Name        stringattr.Type `tfsdk:"name"`
 	Description stringattr.Type `tfsdk:"description"`
 	Permissions strsetattr.Type `tfsdk:"permissions"`
+	Default     boolattr.Type   `tfsdk:"default"`
+	Private     boolattr.Type   `tfsdk:"private"`
 }
 
 func (m *RoleModel) Values(h *helpers.Handler) map[string]any {
@@ -27,6 +32,8 @@ func (m *RoleModel) Values(h *helpers.Handler) map[string]any {
 	stringattr.Get(m.Name, data, "name")
 	stringattr.Get(m.Description, data, "description")
 	strsetattr.Get(m.Permissions, data, "permissions", h)
+	boolattr.Get(m.Default, data, "default")
+	boolattr.Get(m.Private, data, "private")
 
 	// use the name as a lookup key to set the role reference or existing id
 	roleName := m.Name.ValueString()
@@ -46,6 +53,8 @@ func (m *RoleModel) SetValues(h *helpers.Handler, data map[string]any) {
 	stringattr.Set(&m.Name, data, "name")
 	stringattr.Set(&m.Description, data, "description")
 	strsetattr.Set(&m.Permissions, data, "permissions", h)
+	boolattr.Set(&m.Default, data, "default")
+	boolattr.Set(&m.Private, data, "private")
 }
 
 // Matching
@@ -60,4 +69,20 @@ func (m *RoleModel) GetID() stringattr.Type {
 
 func (m *RoleModel) SetID(id stringattr.Type) {
 	m.ID = id
+}
+
+func (m *RoleModel) SetPrivate(private boolattr.Type) {
+	m.Private = private
+}
+
+func (m *RoleModel) IsPrivate() bool {
+	return m.Private.ValueBool()
+}
+
+func (m *RoleModel) SetDefault(def boolattr.Type) {
+	m.Default = def
+}
+
+func (m *RoleModel) IsDefault() bool {
+	return m.Default.ValueBool()
 }
