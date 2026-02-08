04f824cf3980539c16ec288f80b897a55aeb0687
Aviad Lichtenstadt <aviad@descope.com>
2026-02-08 19:41:43 +0200

    Add support for access keys custom attributes (#233)

--- internal/models/project/attributes/attributes.go
+++ internal/models/project/attributes/attributes.go
@@ -7,23 +7,27 @@ import (
 )
 
 var AttributesAttributes = map[string]schema.Attribute{
-	"tenant": listattr.Default[TenantAttributeModel](TenantAttributeAttributes, TenantAttributeModifier),
-	"user":   listattr.Default[UserAttributeModel](UserAttributeAttributes, UserAttributeModifier),
+	"tenant":     listattr.Default[TenantAttributeModel](TenantAttributeAttributes, TenantAttributeModifier),
+	"user":       listattr.Default[UserAttributeModel](UserAttributeAttributes, UserAttributeModifier),
+	"access_key": listattr.Default[AccessKeyAttributeModel](AccessKeyAttributeAttributes, AccessKeyAttributeModifier),
 }
 
 type AttributesModel struct {
-	Tenant listattr.Type[TenantAttributeModel] `tfsdk:"tenant"`
-	User   listattr.Type[UserAttributeModel]   `tfsdk:"user"`
+	Tenant    listattr.Type[TenantAttributeModel]    `tfsdk:"tenant"`
+	User      listattr.Type[UserAttributeModel]      `tfsdk:"user"`
+	AccessKey listattr.Type[AccessKeyAttributeModel] `tfsdk:"access_key"`
 }
 
 func (m *AttributesModel) Values(h *helpers.Handler) map[string]any {
 	data := map[string]any{}
 	listattr.Get(m.Tenant, data, "tenant", h)
 	listattr.Get(m.User, data, "user", h)
+	listattr.Get(m.AccessKey, data, "accessKey", h)
 	return data
 }
 
 func (m *AttributesModel) SetValues(h *helpers.Handler, data map[string]any) {
 	listattr.SetMatchingNames(&m.Tenant, data, "tenant", "displayName", h)
 	listattr.SetMatchingNames(&m.User, data, "user", "displayName", h)
+	listattr.SetMatchingNames(&m.AccessKey, data, "accessKey", "displayName", h)
 }
